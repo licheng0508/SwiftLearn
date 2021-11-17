d0fede69c3a067f3aaa0f4a0e5d9bdee24fc3f4c
licheng <lc900508@sina.com>
2021-11-17 11:02:14 +0800

    commit

---
+++ LearnScene/LearnScene/LearnScene macOS/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  LearnScene macOS
+//
+//  Created by stnts on 2021/11/17.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
