c2c80065e3236700c543c3f3350fe743caf43680
licheng <lc900508@sina.com>
2021-12-22 15:45:37 +0800

    commit

---
+++ LearnSwiftUI/CreatingAmacOSApp/Complete/Landmarks/LandmarksApp.swift
@@ -0,0 +1,35 @@
+/*
+See LICENSE folder for this sample’s licensing information.
+
+Abstract:
+The top-level definition of the Landmarks app.
+*/
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    @StateObject private var modelData = ModelData()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environmentObject(modelData)
+        }
+        #if !os(watchOS)
+        .commands {
+            LandmarkCommands()
+        }
+        #endif
+
+        #if os(watchOS)
+        WKNotificationScene(controller: NotificationController.self, category: "LandmarkNear")
+        #endif
+
+        #if os(macOS)
+        Settings {
+            LandmarkSettings()
+        }
+        #endif
+    }
+}
